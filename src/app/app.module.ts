bfc7cb5a2df7ed4f79f418a85669987b502d9327
Kenneth <palencia.kenneth69@gmail.com>
2022-03-26 22:50:32 +0800

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { FormfieldComponent } from './formfield/formfield.component';
 
+const routes: Routes = [
+  {
+    path :"formfield",
+    component: FormfieldComponent
+  },
+  {
+    path:"",
+    redirectTo: "FormField",
+    pathMatch:"full"
+  },
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormfieldComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      routes
+    ),
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
